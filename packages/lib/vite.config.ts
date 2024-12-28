00cf4da8a7c1a9b8d3f41c09846e5fef7387aef9
rldnd <gi981226@gmail.com>
2024-12-28 23:50:27 +0900

    feat(settings): [WIP] project architecture
    related to: #1

---
+++ packages/lib/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "cjs"],
+    },
+  },
+});
